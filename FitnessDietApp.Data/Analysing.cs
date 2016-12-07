e37ab06d8e39f7eb6dda431ed0ebf3186828b109
bat300 <bat300@yandex.ru>
2016-12-07 18:00:31 +0300

    Adding class Analising

---
+++ FitnessDietApp.Data/Analysing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessDietApp.Data
+{
+    public delegate void Recomendation(string Message);
+    class Analysing
+    {
+        public event Recomendation RecomendationMessage;
+        public void Recomendations()//продумать рекомендации и выводы!
+        {
+            RecomendationMessage("Сообщение");
+        }
+    }
+}
