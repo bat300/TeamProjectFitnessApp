11659cb83d99e4d1c817636b53dbcf54c89b1e74
bat300 <bat300@yandex.ru>
2016-12-17 19:48:39 +0300

    PageOfRecepies added

---
+++ FitnessDietApp.UI/PageForRecepies.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FitnessDietApp.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для PageForRecepies.xaml
+    /// </summary>
+    public partial class PageForRecepies : Page
+    {
+        public PageForRecepies()
+        {
+            InitializeComponent();
+        }
+    }
+}
