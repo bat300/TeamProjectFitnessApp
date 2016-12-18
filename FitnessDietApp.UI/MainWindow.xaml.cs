db117ab8601e6781f164f98c5b38a3ef11772b06
abekbulatova <anabekky@gmail.com>
2016-12-18 21:00:26 +0300

    Added validation

--- FitnessDietApp.UI/MainWindow.xaml.cs
+++ FitnessDietApp.UI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace FitnessDietApp.UI {
     /// <summary>
@@ -22,9 +24,32 @@ namespace FitnessDietApp.UI {
             Start.ChooseOldUser.Click += GoToPageWithPersonalData_Click;
             PersonalData.GoToPageWithRation.Click += GoToPageWithRation_Click;
             Ration.GoToPageOfAnalysis.Click += GoToPageOfAnalysis_Click;
-            Ration.GoToPageWithDiary.Click += GoToPageWithDiary_Click; ;
+            Ration.GoToPageWithDiary.Click += GoToPageWithDiary_Click;
             Diary.GoToPageOfAnalysis.Click += GoToPageOfAnalysis_Click;
             Diary.GoToPageWithRation.Click += GoToPageWithRation_Click;
+
+            PersonalData.Age.TextChanged += ValidatePositiveInt;
+            PersonalData.Weight.TextChanged += ValidatePositiveDouble;
+            PersonalData.Height.TextChanged += ValidatePositiveInt;
+            Ration.ProductWeight.TextChanged += ValidatePositiveInt;
+        }
+
+        private void ValidatePositiveInt(object sender, TextChangedEventArgs e) {
+            var textbox = (TextBox)sender;
+            int n = 0;
+            if ((int.TryParse(textbox.Text, out n)) && (n > 0))
+                textbox.Background = Brushes.White;
+            else
+                textbox.Background = Brushes.Red;
+        }
+
+        private void ValidatePositiveDouble(object sender, TextChangedEventArgs e) {
+            var textbox = (TextBox)sender;
+            double n = 0;
+            if ((double.TryParse(textbox.Text, out n)) && (n > 0))
+                textbox.Background = Brushes.White;
+            else
+                textbox.Background = Brushes.Red;
         }
 
         private void GoToPageWithDiary_Click(object sender, RoutedEventArgs e) {
